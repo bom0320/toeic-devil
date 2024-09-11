f13be1d0c33694d83ee77653bf62353a1f5b81aa
bom0320 <167315197+bom0320@users.noreply.github.com>
2024-09-11 14:04:57 +0900

    Toeic-Devil

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 
 const DayList = () => {
   const [days, setDays] = useState([]);
+  // 여기 컴포넌트 상태 days 는 자체적으로 data.json 의 데이터를 나타내는 것이 아니라, HTTP 요청을 통해
+  // 그 데이터를 가져 온 후 상태로 관리하게 된다.
 
   const getData = async () => {
     try {
@@ -21,6 +23,10 @@ const DayList = () => {
   useEffect(() => {
     getData();
   }, []);
+
+  // getData,useEffect : 이 코드들에 의해 컴포넌트 상태 days 는 처음에는 빈 배열로 초기화되어 있었지만,
+  // HTTP 요청을 통해 data.json 파일의 days 데이터를 서버에서 가져오고, 그 데이터를 상태로 저장하면서
+  // data.json 의 days 와 연결된 것 처럼 동작하는 것이다. 
 
   return (
     <ul className="list_day">
